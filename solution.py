731e2ffce430fcf2198d44af7f86921f423d45e6
semenkulemin <55997982+semenkulemin@users.noreply.github.com>
2023-04-13 16:13:51 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
-from statsmodels.stats.proportion import proportions_ztest
+# from statsmodels.stats.proportion import proportions_ztest
+from scipy import stats
 
 chat_id = 341395919 # Ваш chat ID, не меняйте название переменной
 
@@ -11,5 +12,10 @@ def solution(x_success: int,
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    z_score, p_value = proportions_ztest([x_success, y_success], [x_cnt, y_cnt], alternative='larger')
-    return p_value < 0.1
+#     z_score, p_value = proportions_ztest([x_success, y_success], [x_cnt, y_cnt], alternative='larger')
+#     return p_value < 0.1
+    alpha = 0.1
+    u = stats.norm.ppf((1-alpha)/2+0.5)
+    px = x_success / x_cnt
+    py = y_success / y_cnt
+    return py < px - u * np.sqrt(px * (1 - px) / x_cnt)
