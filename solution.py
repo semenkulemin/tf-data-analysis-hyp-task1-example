fbb68dcd2355b57260d1de07d08aa1340b6ed7f8
semenkulemin <55997982+semenkulemin@users.noreply.github.com>
2023-04-13 14:34:44 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,8 +1,8 @@
 import pandas as pd
 import numpy as np
+from statsmodels.stats.proportion import proportions_ztest
 
-
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 341395919 # Ваш chat ID, не меняйте название переменной
 
 def solution(x_success: int, 
              x_cnt: int, 
