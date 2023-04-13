cd904c5061a7a5956b3c706cf1b75b2d328f0ee7
semenkulemin <55997982+semenkulemin@users.noreply.github.com>
2023-04-13 16:47:09 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
-# from statsmodels.stats.proportion import proportions_ztest
-from scipy import stats
+from statsmodels.stats.proportion import proportions_ztest
+# from scipy import stats
 
 chat_id = 341395919 # Ваш chat ID, не меняйте название переменной
 
@@ -14,13 +14,8 @@ def solution(x_success: int,
     # Не меняйте название функции и её аргументы
 #     z_score, p_value = proportions_ztest([x_success, y_success], [x_cnt, y_cnt], alternative='larger')
 #     return p_value < 0.1
-#     alpha = 0.1
-#     u = stats.norm.ppf((1-alpha)/2+0.5)
-#     px = x_success / x_cnt
-#     py = y_success / y_cnt
-#     return py < px - u * np.sqrt(px * (1 - px) / x_cnt)
-    _,p_val = proportions_ztest([x_success,y_success],[x_cnt,y_cnt], alternative = 'smaller')
-    if p_val > 0.1:
+    _,p_value = proportions_ztest([x_success,y_success],[x_cnt,y_cnt], alternative = 'smaller')
+    if p_value > 0.1:
         return False
     else:
         return True 
